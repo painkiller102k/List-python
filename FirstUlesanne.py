bb49deb53fc11bf07c3db3d30a37bbf17122aecc
painkiller102k <mstislavlandsh@gmail.com>
2025-03-11 22:46:26 +0200

    first ulesanne done

--- FirstUlesanne.py
+++ FirstUlesanne.py
@@ -17,9 +17,10 @@ while True:
     print("9 - Asendada täht")  # Заменить букву в списке
     print("10 - Nimekirja segamine")  # Перемешать список
     print("11 - Loendi algse järjekorra taastamine") #Восстановить исходный порядок списка
-    print("12 - Exit")  # Завершить программу
+    print("12 - Muuda kõik tähed suureks või väikseks") # Сделать все буквы заглавными или маленькими.
+    print("13 - Exit")  # Завершить программу
 
-    choice = input("Выберите действие: ")
+    choice = input("Valige tegevus: ") #выберите действие
     if choice == "1":
         print(word_list)
     elif choice == "2":
@@ -27,66 +28,77 @@ while True:
         print(word_list)
     elif choice == "3":
         while True:
-            täht = input("Введите букву: ")
+            täht = input("Sisestage täht: ") #введите букву
             try:
                 print(word_list.index(täht))
                 break
             except ValueError:
-                print("Буква не найдена, попробуйте снова.")
+                print("Täht ei leitud, proovige uuesti.") #буква не найдена, попробуйте снова
     elif choice == "4":
         while True:
-            täht = input("Введите букву: ")
+            täht = input("Sisestage täht: ") #введите букву
             try:
                 word_list.remove(täht)
                 print(word_list)
                 break
             except ValueError:
-                print("Буква не найдена, попробуйте снова.")
+                print("Täht ei leitud, proovige uuesti.") #Буква не найдена, попробуйте снова.
     elif choice == "5":
         print(len(word_list))
     elif choice == "6":
         while True:
-            täht = input("Введите букву которую хочешь добавить: ")
-            a = input("Введите номер позиции: ")
+            täht = input("Sisestage täht, mida soovite lisada: ") #Введите букву которую хочешь добавить
+            a = input("Sisestage positsiooni number: ") #Введите номер позиции
             try:
                 word_list.insert(int(a), täht)
                 print(word_list)
                 break
             except ValueError:
-                print("Буква не найдена, попробуйте снова.")
+                print("Täht ei leitud, proovige uuesti.") #Буква не найдена, попробуйте снова
     elif choice == "7":
         word_list.clear()
         print(word_list)
     elif choice == "8":
         while True:
-            täht = input("Введите букву: ")
+            täht = input("Sisestage täht ") #Введите букву
             try:
                 print(word_list.count(täht))
                 break
             except ValueError:
-                print("Буква не найдена, попробуйте снова.")
+                print("äht ei leitud, proovige uuesti.") #Буква не найдена, попробуйте снова.
     elif choice == "9":
         while True:
-            old = input("Введите букву, которую хотите заменить: ")
-            new = input("Введите новую букву: ")
+            old = input("Sisestage täht, mida soovite asendada: ") #Введите букву, которую хотите заменить:
+            new = input("Sisestage uus täht: ") #Введите новую букву:
             try:
                 index = word_list.index(old)
                 word_list[index] = new
                 print(word_list)
                 break
             except ValueError:
-                print("Буква не найдена, попробуйте снова.")
+                print("Täht ei leitud, proovige uuesti.") #Буква не найдена, попробуйте снова.
     elif choice == "10":
         random.shuffle(word_list)
         print(word_list)
     elif choice == "11":
         word_list = original_list.copy()
-        print("Список восстановлен:", word_list)
-    elif choice == "12":
-        print("Вы вышли из программы")
+        print("Nimekiri on taastatud:", word_list) #Список восстановлен:
+    elif choice =="12":
+        print("1 - Muuda kõik tähed suureks ")  #все буквы в списке заглавные
+        print("2 - Muuda kõik tähed väikseks ")  #все буквы в списке маленькие
+        choice2 = input("Valige tegevus: ")  # Выберите действие
+        if choice2 == "1":
+            word_list = [char.upper() for char in word_list]
+        elif choice2 == "2":
+            word_list = [char.lower() for char in word_list]
+        else:
+            print("Vale valik")  # Неверный выбор  
+        print(word_list)
+    elif choice == "13":
+        print("Sa oled programmist väljas!") #Вы вышли из программы
         exit()
     else:
-        print("Неверный выбор")
+        print("Vale valik") #неверный выбор
         break
 
-print("Программа завершена")
+print("Программа завершена") 
